89ff0a3107ef42fc7f6b2de4ba923e623998eb63
PChabowski <p.chabowski1@gmail.com>
2026-03-09 09:50:29 +0100

    feat: Enhance PeerManager with improved connection handling and logging

--- src/utils/peer.js
+++ src/utils/peer.js
@@ -11,53 +11,92 @@ export class PeerManager {
 
     initHost() {
         this.isHost = true;
-        this.peer = new Peer();
+        this.peer = new Peer({
+            config: {
+                iceServers: [
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { urls: 'stun:stun1.l.google.com:19302' },
+                    { urls: 'stun:stun2.l.google.com:19302' },
+                    { urls: 'stun:stun3.l.google.com:19302' },
+                    { urls: 'stun:stun4.l.google.com:19302' }
+                ]
+            }
+        });
 
         this.peer.on('open', (id) => {
+            console.log('Host Peer opened with ID:', id);
             this.peerId = id;
             if (this.onOpenCallback) this.onOpenCallback(id);
         });
 
         this.peer.on('connection', (conn) => {
+            console.log('Host received connection incoming!');
             this.connection = conn;
             this._setupConnection();
-            if (this.onConnectionCallback) this.onConnectionCallback(conn);
+            // DO NOT call onConnectionCallback(conn) here. Must wait for connection.on('open')
         });
 
         this.peer.on('error', (err) => {
-            console.error('Peer error:', err);
+            console.error('Peer error on Host:', err);
         });
     }
 
     connectToHost(id) {
+        console.log('Client attempting to connect to host with ID:', id);
         this.isHost = false;
-        this.peer = new Peer();
+        this.peer = new Peer({
+            config: {
+                iceServers: [
+                    { urls: 'stun:stun.l.google.com:19302' },
+                    { urls: 'stun:stun1.l.google.com:19302' },
+                    { urls: 'stun:stun2.l.google.com:19302' },
+                    { urls: 'stun:stun3.l.google.com:19302' },
+                    { urls: 'stun:stun4.l.google.com:19302' }
+                ]
+            }
+        });
 
         this.peer.on('open', () => {
-            this.connection = this.peer.connect(id);
+            console.log('Client Peer opened, initiating data connection to Host.');
+            this.connection = this.peer.connect(id, { reliable: true });
             this._setupConnection();
         });
 
         this.peer.on('error', (err) => {
-            console.error('Peer error:', err);
+            console.error('Peer error on Client:', err);
         });
     }
 
     _setupConnection() {
-        this.connection.on('open', () => {
+        console.log('Setting up DataConnection events. Current open state:', this.connection.open);
+        
+        const openHandler = () => {
+            console.log('DataConnection opened successfully!');
             if (this.onConnectionCallback) this.onConnectionCallback(this.connection);
-        });
+        };
+
+        if (this.connection.open) {
+            openHandler();
+        } else {
+            this.connection.on('open', openHandler);
+        }
 
         this.connection.on('data', (data) => {
+            console.log('Received data:', data);
             if (this.onDataCallback) this.onDataCallback(data);
         });
 
         this.connection.on('close', () => {
             console.log('Connection closed');
         });
+        
+        this.connection.on('error', (err) => {
+            console.error('Connection error:', err);
+        });
     }
 
     disconnect() {
+        console.log('Disconnecting Peer and DataConnection...');
         if (this.connection) {
             this.connection.close();
             this.connection = null;
@@ -69,7 +108,6 @@ export class PeerManager {
         this.peerId = null;
         this.isHost = false;
         
-        // wyczyszczenie callbacków aby nie dublować przycisków
         this.onDataCallback = null;
         this.onOpenCallback = null;
         this.onConnectionCallback = null;
@@ -78,6 +116,8 @@ export class PeerManager {
     send(data) {
         if (this.connection && this.connection.open) {
             this.connection.send(data);
+        } else {
+            console.error('Cannot send data; connection is not open.', data);
         }
     }
 
